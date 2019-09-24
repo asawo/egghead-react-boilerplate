767d5928e742c94d90110f89d6b0d788c69f2db8
asawo <asawo@mercari.com>
2019-09-24 20:13:43 +0900

    target specific browsers with babel-preset-env and the babel pollyfill

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App'
 import './styles.css'
+import '@babel/polyfill'
 
 ReactDom.render(<App/>, document.getElementById('app'))
