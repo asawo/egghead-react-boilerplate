1ff6996ddeb9e7699e16138ec460d293d7a4bc29
asawo <asawo@mercari.com>
2019-09-29 15:56:53 +0900

    added react-axe to check accessibility issues

--- src/App.js
+++ src/App.js
@@ -21,9 +21,7 @@ class App extends React.Component {
     return (
       <div>
         <h1>Hello World</h1>
-        <h2 className={count > 10 ? 'warning' : null}>
-          Count: {this.state.count}
-        </h2>
+        <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
         {count > 10 ? (
